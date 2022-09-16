0234e785f1fc43d0d14edc5b54e99a2a67a8eeda
here and now <110921333+Entpler@users.noreply.github.com>
2022-09-16 13:19:03 +0900

    JDBC 게시판 만들기 예제_최근글검색, 날짜 검색 기능 추가_service클래스

--- BoardService.java
+++ BoardService.java
@@ -1,161 +1,89 @@
-package service;
+package com.kh.model;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import model.vo.Board;
-import model.vo.dao.BoardDao;
+import com.kh.common.BoardTemplate;
+
+
 
 public class BoardService {
 
+	String classname = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	String id = "JDBC";
+	String password = "JDBC";
+
 	public int createPost(Board b) {
 		int result = 0;
-		Connection conn = null;
+
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
+			Class.forName(classname);
+			Connection conn = DriverManager.getConnection(url, id, password);
 			result = new BoardDao().createPost(conn, b);
 			if (result > 0) {
-				conn.commit();
-			} else {
-				conn.rollback();
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return result;
-	}
+				BoardTemplate.commit(conn);
+				
+			}else
+				BoardTemplate.rollback(conn);
 
-	public int modifyPost(Board b) {
-		int result = 0;
-		Connection conn = null;
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-			result = new BoardDao().modifyPost(conn, b);
-			if (result > 0) {
-				conn.commit();
-			} else {
-				conn.rollback();
-			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
+
 			e.printStackTrace();
-		} finally {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 
 		return result;
 	}
 
 	public ArrayList<Board> selectAll() {
-		Connection conn = null;
-		ArrayList<Board> list = null;
+		ArrayList<Board> list = new ArrayList<>();
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
+			Class.forName(classname);
+			Connection conn = DriverManager.getConnection(url, id, password);
 			list = new BoardDao().selectAll(conn);
-			if (list.isEmpty()) {
-				conn.commit();
-			} else {
-				conn.rollback();
-			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				conn.close();
+		}
+		return list;
+	}
+
+	public ArrayList<Board> searchForTopN(int N) {
+		ArrayList<Board> list = new ArrayList<>();
+		try {
+			Class.forName(classname);
+			Connection conn = DriverManager.getConnection(url, id, password);
+			list = new BoardDao().selectAll(conn, N);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public ArrayList<Board> selectDate(String dd) {
+		
+			 ArrayList<Board> list = null;
+			 Connection conn = null;
+			 try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
+				list = new BoardDao().selectDate(conn, dd);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		return list;
-	}
-
-	public int deltePost(int bno) {
-		Connection conn = null;
-		int result = 0;
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-			result = new BoardDao().deletePost(conn, bno);
-			if (result > 0) {
-				conn.commit();
-			} else {
-				conn.rollback();
+				return list;
 			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
 		}
-		return result;
-	}
 
-	public ArrayList<Board> searchForTitle(String keyword) {
-		ArrayList<Board> list = null;
-		Connection conn = null;
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-			list = new BoardDao().searForTitle(conn, keyword);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
-
-	public ArrayList<Board> searchForUser(String userId) {
-		ArrayList<Board> list = null;
-		Connection conn = null;
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-			list = new BoardDao().searchForUser(conn, userId);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return list;
-
-	}
-
-	public ArrayList<Board> selectDate(int writed_date) {
-	 ArrayList<Board> list = null;
-	 Connection conn = null;
-	 try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-		list = new BoardDao().selectDate(conn,  writed_date);
-	} catch (ClassNotFoundException e) {
-		e.printStackTrace();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-		return list;
-	}
-}
+		
